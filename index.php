843c5fae6deffb96c862c9bd354d9ae5e26f64e9
Vitaliy Moskalenko <to-@mail.ru>
2020-06-11 11:24:49 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,442 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="icon" href="img/icon/favicon.ico" type="image/x-icon">
+
+	<title>Ray Ban</title>
+
+	<link href='css/lightbox.css' rel='stylesheet' type='text/css'>
+	<link href='css/main.css' rel='stylesheet' type='text/css'>
+</head>  
+<body>
+
+<header class="fixed">
+  <div class="container">
+    <div class="header-row">
+      <div class="header-logo">
+        <a href=""></a>
+      </div>
+      <nav id="main-nav" class="topnav">
+      	<ul id="menu">
+        	<li><a href="" class="top-anchor" id="products-section_link">Цены</a></li>
+        	<li><a href="" class="top-anchor" id="orders-section_link">Приемущества</a></li>
+        	<li><a href="" class="top-anchor" id="delivery-section_link">Доставка и оплата</a></li>
+        	<li><a href="" class="top-anchor" id="delivery-section_link">Гарантия</a></li>
+        	<li><a href="" class="top-anchor" id="callback-form_link">Заказать звонок</a></li>                
+      	</ul>
+      	<a href="javascript:void(0);" class="burger" onclick="toggleBurger()"></a>
+      </nav>	  	
+    </div>
+  </div>
+</header>
+
+<div class="wrapper">
+	<section class="banners-section">
+		<div id="main-banner">
+			<img src="img/header.png" alt="" />
+			<div id="main-banner-info">
+				<img src="img/logo4.png" alt="" />
+				<p>Распродажа очков RAY-BAN</p>
+				<h2>С ДОСТАВКОЙ ПО ВСЕЙ РОССИИ</h2>	
+			</div>		
+		</div>	
+	</section>
+	<section id="products-section">
+		<div class="container offers">
+			<h2>ОЧКИ УНИСЕКС</h2>
+			<div class="offer-panel">
+				<div class="offer-panel-block">
+					<img src="img/catalog/1/1.jpg" alt="" />
+					<p class="right old-price"><span class="line-through">3 900 руб.</span>&nbsp;&nbsp;<span class="red-text">- 50%</span></p>
+					<p class="right"><span class="model">WAYFARER</span><span class="red-price">1 950 руб.</span></p>
+					<p class="offer-text">Классическия модель в черной глянцевой пластиковой оправе с темно-зелеными линзами.
+						Благодаря применению шарнирной системы оправа характеризуется долгим сроком службы.</p>
+					<a href="" class="more" id="1_more">Подробнее</a>	
+				</div>				
+				<div class="offer-panel-block">
+					<img src="img/catalog/2/1.jpg" alt="" />
+					<p class="right old-price"><span class="line-through">6 900 руб.</span>&nbsp;&nbsp;<span class="red-text">- 50%</span></p>
+					<p class="right"><span class="model">CATS</span><span class="hit red-price">ХИТ</span><span class="red-price">3 450 руб.</span></p>
+					<p class="offer-text">Крупные коричневые градиентные линзы из стекла отлично скроют и защитят ваши глаза на солнечном пляже,
+					а переливающиеся пятнистые леопардовые дужки подчеркнут образ.</p>
+					<a href="" class="more" id="2_more">Подробнее</a>
+				</div>
+				<div class="offer-panel-block">
+					<img src="img/catalog/3/1.jpg" alt="" />
+					<p class="right old-price"><span class="line-through">4 900 руб.</span>&nbsp;&nbsp;<span class="red-text">- 50%</span></p>
+					<p class="right"><span class="model">ROUND METAL</span><span class="red-price">2 350 руб.</span></p>
+					<p class="offer-text">Классическая модель в металлической оправе матового серого цвета. Линзы натурального зеленого оттенка
+					 с технологией защитного покрытия G15.</p>
+					<a href="" class="more" id="3_more">Подробнее</a> 
+				</div>		
+			</div>
+		</div>
+		<div class="popup" id="product_1">
+			<div class="closeme"></div>
+			<div class="popup-left">
+				<img src="img/catalog/1/1.jpg" alt="" />
+				<p>
+					<a class="lightbox" href="img/catalog/1/f1.jpg"><img src="img/catalog/1/1.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/1/f2.jpg"><img src="img/catalog/1/2.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/1/f3.jpg"><img src="img/catalog/1/3.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/1/f4.jpg"><img src="img/catalog/1/4.jpg" height="70px" width="100px" alt=""/></a>				
+				</p>
+			</div>
+			<div class="popup-right">
+				<p><span class="model red-text">WAYFARER</span></p>
+				<p class="old-price"><span class="line-through">3 900 руб.</span>&nbsp;&nbsp;<span class="red-text">- 50%</span></p>
+				<p><span class="price">1 950 руб.</span><span class="price-order">ЗАКАЗАТЬ</span></p>
+				<table>
+					<tr>
+						<td>Цвет оправы:</td>
+						<td>черный</td>
+					</tr>
+					<tr>	
+						<td>Цвет линз:</td>
+						<td>зеленый</td>
+					</tr>
+					<tr>						
+						<td>Форма очков:</td>
+						<td>трапеция</td>
+					</tr>
+					<tr>						
+						<td>Материал оправы:</td>
+						<td>пластик</td>
+					</tr>
+					<tr>						
+						<td>Серия модели:</td>
+						<td>RB 2140</td>
+					</tr>
+					<tr>						
+						<td>Производитель:</td>
+						<td>Италия</td>
+					</tr>
+					<tr>
+						<td>Особенности:</td>
+						<td>Линзы G15</td>	
+					</tr>
+				</table>
+				<h5>Описание модели</h5>
+				<p>Солнцезащитные очки Wayfarer, классическая хитовая модель в черной глянцевой пластиковой оправе с темно-зелеными линзами. на дужках есть наклеенный значок бренда, на правой линзе надпись, а на левой - гравировка букв RB. В комплектацию входят: футляр черного или коричневого оттенка, салфетка, минибуклет, коробка.</p>
+			</div>
+		</div>
+		<div class="popup" id="product_2">
+			<div class="closeme"></div>
+			<div class="popup-left">
+				<img src="img/catalog/2/1.jpg" alt="" />
+				<p>
+					<a class="lightbox" href="img/catalog/2/f1.jpg"><img src="img/catalog/2/1.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/2/f2.jpg"><img src="img/catalog/2/2.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/2/f3.jpg"><img src="img/catalog/2/3.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/2/f4.jpg"><img src="img/catalog/2/4.jpg" height="70px" width="100px" alt=""/></a>				
+				</p>
+			</div>
+			<div class="popup-right">
+				<p><span class="model red-text">CATS</span></p>
+				<p class="old-price"><span class="line-through">6 900 руб.</span>&nbsp;&nbsp;<span class="red-text">- 50%</span></p>
+				<p><span class="price">3 450 руб.</span><span class="price-order">ЗАКАЗАТЬ</span></p>
+				<table>
+					<tr>
+						<td>Цвет оправы:</td>
+						<td>черный</td>
+					</tr>
+					<tr>	
+						<td>Цвет линз:</td>
+						<td>зеленый</td>
+					</tr>
+					<tr>						
+						<td>Форма очков:</td>
+						<td>трапеция</td>
+					</tr>
+					<tr>						
+						<td>Материал оправы:</td>
+						<td>пластик</td>
+					</tr>
+					<tr>						
+						<td>Серия модели:</td>
+						<td>RB 2140</td>
+					</tr>
+					<tr>						
+						<td>Производитель:</td>
+						<td>Италия</td>
+					</tr>
+					<tr>
+						<td>Особенности:</td>
+						<td>Линзы G15</td>	
+					</tr>
+				</table>
+				<h5>Описание модели</h5>
+				<p>Солнцезащитные очки Wayfarer, классическая хитовая модель в черной глянцевой пластиковой оправе с темно-зелеными линзами. на дужках есть наклеенный значок бренда, на правой линзе надпись, а на левой - гравировка букв RB. В комплектацию входят: футляр черного или коричневого оттенка, салфетка, минибуклет, коробка.</p>				
+			</div>
+		</div>
+		<div class="popup" id="product_3">
+			<div class="closeme"></div>
+			<div class="popup-left">
+				<img src="img/catalog/3/1.jpg" alt="" />
+				<p>
+					<a class="lightbox" href="img/catalog/3/f1.jpg"><img src="img/catalog/3/1.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/3/f2.jpg"><img src="img/catalog/3/2.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/3/f3.jpg"><img src="img/catalog/3/3.jpg" height="70px" width="100px" alt=""/></a>
+					<a class="lightbox" href="img/catalog/3/f4.jpg"><img src="img/catalog/3/4.jpg" height="70px" width="100px" alt=""/></a>				
+				</p>				
+			</div>
+			<div class="popup-right">
+				<p><span class="model red-text">ROUND METAL</span></p>
+				<p class="old-price"><span class="line-through">4 000 руб.</span>&nbsp;&nbsp;<span class="red-text">- 50%</span></p>
+				<p><span class="price">2 350 руб.</span><span class="price-order">ЗАКАЗАТЬ</span></p>
+				<table>
+					<tr>
+						<td>Цвет оправы:</td>
+						<td>черный</td>
+					</tr>
+					<tr>	
+						<td>Цвет линз:</td>
+						<td>зеленый</td>
+					</tr>
+					<tr>						
+						<td>Форма очков:</td>
+						<td>трапеция</td>
+					</tr>
+					<tr>						
+						<td>Материал оправы:</td>
+						<td>пластик</td>
+					</tr>
+					<tr>						
+						<td>Серия модели:</td>
+						<td>RB 2140</td>
+					</tr>
+					<tr>						
+						<td>Производитель:</td>
+						<td>Италия</td>
+					</tr>
+					<tr>
+						<td>Особенности:</td>
+						<td>Линзы G15</td>	
+					</tr>
+				</table>
+				<h5>Описание модели</h5>
+				<p>Солнцезащитные очки Wayfarer, классическая хитовая модель в черной глянцевой пластиковой оправе с темно-зелеными линзами. на дужках есть наклеенный значок бренда, на правой линзе надпись, а на левой - гравировка букв RB. В комплектацию входят: футляр черного или коричневого оттенка, салфетка, минибуклет, коробка.</p>				
+			</div>
+		</div>				
+	</section>
+	<section id="orders-section">
+		<div class="container">
+			<div class="orders">
+				<div class="orders-panel">
+					<div class="orders-panel-icon">					
+						<img src="img/icon/5.png" alt="" />
+					</div>
+					<p>Круглосуточный прием заявок.</p>
+				</div>
+				<div class="orders-panel">
+					<div class="orders-panel-icon">
+						<img src="img/icon/4.png" alt="" />
+					</div>
+					<p>Перед отправкой очки проходят 100% проверку.</p>	
+				</div>
+				<div class="orders-panel">
+					<div class="orders-panel-icon">
+						<img src="img/icon/7.png" alt="" />
+					</div>	
+					<p>Доставка почтой России 1-классом.</p>
+				</div>
+				<div class="orders-panel">
+					<div class="orders-panel-icon">
+						<img src="img/icon/6.png" alt="" />
+					</div>
+					<p>Постоянные акции и скидки.</p>
+				</div>
+				<div class="callback">ЗАКАЗАТЬ ЗВОНОК</div>
+			</div>
+		</div>
+	</section>
+	<section class="delivery-section" id="delivery-section">
+		<div class="container delivery">
+			<div class="delivery-panel">
+				<img src="img/icon/1.png" alt="" />
+				<div>
+					<h5>ДОСТАВКА</h5>
+					<p>Доставка почтой в течение 1-10 рабочих дней. Стоимость доставки товара от 300 р.</p>
+				</div>
+			</div>
+			<div class="delivery-panel">
+				<img src="img/icon/2.png" alt="" />
+				<div>
+					<h5>ОПЛАТА</h5>
+					<p>Никаких предоплат! Оплата заказов осуществляется во время получения по почте.</p>
+				</div>
+			</div>
+			<div class="delivery-panel">
+				<img src="img/icon/3.png" alt="" />
+				<div>
+					<h5>ГАРАНТИИ</h5>
+					<p>Вы вправе отказаться от покупки в течение 14 дней с момента получения заказа.</p>
+				</div>
+			</div>	
+			<div class="hide-details">СКРЫТЬ ПОДРОБНОСТИ</div>
+		</div>
+	</section>
+	<section class="delivery-section-2">
+		<div class="container delivery-2">		
+			<div class="delivery-2-left">
+				<img src="img/1.png" alt="" />
+				<div class="delivery-2-text">
+					<h2 class="red-text">ГАРАНТИЯ</h2>
+					<p class="red-text">Оплата при получении</p>
+					<ul>
+						<li>Мы не берем с вас предоплату.</li>
+						<li>Оплата заказа осуществляется при покупке в почтовом отделении.</li>
+					</ul>	
+					<p class="red-text">Гарантии на товар</p>
+					<ul>
+						<li>Перед отправкой товары проходят 100% проверку.</li>
+						<li>Мы вернем вам деньги, если что-то окажется не так.</li>
+						<li>Мы соблюдаем закон "О защите прав потребителей"</li>
+						<li>Вы в праве отказаться от покупки в течение 14 дней с момента получения заказа, независимо от причины возврата.</li>
+					</ul>					
+					<p class="red-text">Возврат денег</p>
+					<ul>
+						<li>Вам необходимо обратиться в службу поддержки клиентов.</li>
+						<li>Выслать нам купленый товар.</li>						
+					</ul>
+					<p>Мы вернем вам деньги за заказ, за исключением расходов на доставку.</p>
+				</div>
+			</div>
+			<div class="delivery-2-right">
+				<div class="delivery-2-text">
+					<h2 class="red-text">ДОСТАВКА И ОПЛАТА</h2>
+					<p class="red-text">Доставка почтой России</p>
+					<p>Мы вам доверяем и не требуем с васпредоплаты. Мы уважаем ваше время, и поэтому все заказы отправляем только бандеролями 1 класса.
+					Вы можете забрать и оплатить свои новые очки на почте в любое удобное для вас время.</p>
+					<ul>
+						<li>Стоимость доставки товара от 300 р.</li>						
+					</ul>	
+					<p class="red-text">Сроки доставки Почты России</p>
+					<p>Мы отправим ваш заказ уже сегодня 1 классом. Очки дойдут до вас быстро и без единой царапинки.</p>
+					<ul>
+						<li>Доставка займет 1-10 рабочих дней.</li>
+					</ul>
+					<p>Чтобы ускорить прибытие вашего заказа, мы отправляем бандероли с Главпочтампта два раза в день, включая выходные дни.</p>					
+					<p class="red-text">Оплата наложенным платежом</p>
+					<ul>
+						<li>Оплата осуществляется в почтовом отделении при получении заказа.</li>
+					</ul>
+					<p class="red-text">Условия доставки почтой России</p>
+					<p>В момент отправки каждому заказу присваивается номер. По номеру можно отследить доставку посылки на сайте Почты России. Мы незамедлительно вышлем номер на ваш e-mail или СМС-сообщением. Получить заказ можно в почтовом отделении, индекс которого был указан при оформлении заказа. Уведомление о доставке заказа придет по вашему адресу. Если уведомление вам не поступило, то через 14 дней сходите на почту самостоятельно. Скорее всего ваши очки уже ждут вас на почте. Для получения заказа на почте возьмите с собой паспорт. Срок хранения заказа в почтовом отделении составляет 1 месяц с момента поступления. Просим вас своевременно получить пришедший заказ. В настоящее время мы осуществляем доставку только по России.</p>
+				</div>				
+				<img src="img/2.png" alt="" />
+			</div>
+		</div>	
+	</section>
+	<section class="white-space"></section>
+	<section class="footer">
+		<div class="container">
+			<div class="cleaner">
+				<img src="img/histilka.png" alt="" />
+				<div>
+					<h5>ЧИСТИЛКА В ПОДАРОК</h5>
+					<p>Мы дарим вам прекрасную чистилку для очков, которая очистит все и приведет в идеальное состоянии стекла. Не оставит царапин и каких-либо разводов. Чистилка - отличный помощник для ухода за вашим зрением, ведь в грязные и замазанные очки просто невозможно смотреть.
+				</div>
+			</div>
+			<div class="discount">
+				<h2 class="red-text">50%</h2>
+				<h3>НЕ ТЯНИТЕ КОТА ЗА ХВОСТ</h3>
+				<P>Закажите очки Ray-Ban прямо сейчас и получите скидку 50%.
+			</div>
+			<div class="callback-form" id="callback-form">
+				<form action="somehandler" method="get">
+					<label for="phone">Мобильный номер телефона</label>
+					<input type="tel" name="phone" id="phone" placeholder="Номер телефона" required />
+					<label for="name">Как к Вам обращаться</label>
+					<input type="text" name="name" id="name" placeholder="Ваше имя" required="">
+					<input type="submit" value="ЗАКАЗАТЬ" />
+				</form>	
+			</div>
+		</div>
+	</section>
+	<section class="footer-2">
+		<div class="container">
+			<div class="contacts red-text">
+				ООО "ВЕКТОР" ОГРН 1168658094086.<br/>
+				ИНН: 8658479619.<br/>
+				620109, г.Екатеринбург, ул. Крауля д.9а, о.407
+			</div>
+			<div class="policy">
+				<a href="/">Политика конфедициальности</a>
+				<a href="/">Пользовательское соглашение</a>
+			</div>
+			<div class="copyright">
+				<p>Cайт разработан компанией JUNZER</p><br/>
+				<p>&copy; 2020 Lovely shopping ВСЕ ПРАВА ЗАЩИЩЕНЫ
+			</div>
+		</div>
+	</section>
+</div>	 <!-- wrapper -->
+	
+<script type="text/javascript" src="js/jquery.min.js" ></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+<script type="text/javascript">
+function toggleBurger() {
+	var x = document.getElementById("main-nav");
+	
+	if (x.className === "topnav") 
+    	x.className += " responsive";
+  	else 
+    	x.className = "topnav";  
+} 
+
+$(document).ready(function() {
+	// Catalog lightbox images
+	$(".lightbox").lightBox({
+		overlayBgColor: '#FFF',
+		overlayOpacity: 0.6,
+		containerResizeSpeed: 350,
+    });
+
+	// Top nav links
+	$('.top-anchor').click(function(e) {
+		e.preventDefault(); 
+		var scrollId = this.id.substr(0, this.id.indexOf('_'));  
+		$('html, body').animate({
+			scrollTop: $('#' + scrollId).offset().top 
+		}, 500);
+	});
+
+	// Product detail info
+	$('.more').click(function(e) {
+		e.preventDefault();
+		$('.popup').fadeOut(700);
+		var prodId = this.id.substr(0, this.id.indexOf('_'));
+		$('#product_' + prodId).fadeIn(700);
+	});
+
+	$('.closeme').click(function() {
+		$('.popup').fadeOut(700);
+	});
+
+	// Toggle delivery details
+	$('.hide-details').click(function() {
+		$(this).toggleClass('hidden');
+
+		if($(this).hasClass('hidden')) {
+			$('.delivery-section-2').fadeOut(700);
+			$(this).text('ПОКАЗАТЬ ПОДРОБНОСТИ');
+		}
+		else {
+			$('.delivery-section-2').fadeIn(700);
+			$(this).text('СКРЫТЬ ПОДРОБНОСТИ'); 			
+		}
+	});
+
+    // Callback button downscroll
+	$('.callback, .price-order').click(function() {
+		$('html, body').animate({
+			scrollTop: $('.callback-form').offset().top 
+		}, 500);
+	});
+});
+</script>
+
+</body>
+</html>
